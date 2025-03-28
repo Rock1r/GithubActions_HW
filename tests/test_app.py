e920d851c63928187deca6b46a8f3cc41d4a1dc4
Rostyslav <146858503+Rock1r@users.noreply.github.com>
2025-03-28 19:22:15 +0200

    Fixed import into test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,26 +1,26 @@
-from myapp.app import app
+from myapp.app import add, substract, multiply, divide
 
 def test_add():
-    assert app.add(2, 3) == 5
-    assert app.add(-1, 1) == 0
-    assert app.add(0, 0) == 0
-    assert app.add(-1, -1) == -2
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
+    assert add(0, 0) == 0
+    assert add(-1, -1) == -2
 
 def test_subtract():
-    assert app.substract(5, 3) == 2
-    assert app.substract(-1, 1) == -2
-    assert app.substract(0, 0) == 0
-    assert app.substract(-1, -1) == 0
+    assert substract(5, 3) == 2
+    assert substract(-1, 1) == -2
+    assert substract(0, 0) == 0
+    assert substract(-1, -1) == 0
 
 def test_multiply():
-    assert app.multiply(2, 3) == 6
-    assert app.multiply(-1, 1) == -1
-    assert app.multiply(0, 5) == 0
-    assert app.multiply(-1, -1) == 1
+    assert multiply(2, 3) == 6
+    assert multiply(-1, 1) == -1
+    assert multiply(0, 5) == 0
+    assert multiply(-1, -1) == 1
 
 def test_divide():
-    assert app.divide(6, 3) == 2
-    assert app.divide(-1, 1) == -1
-    assert app.divide(0, 1) == 0
-    assert app.divide(-1, -1) == 1
+    assert divide(6, 3) == 2
+    assert divide(-1, 1) == -1
+    assert divide(0, 1) == 0
+    assert divide(-1, -1) == 1
 
